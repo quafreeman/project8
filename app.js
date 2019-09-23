6c490164d8b7661dcf6d4ed5dc65d4bbd8d2406b
Quadria Freeman <quadria.freeman@verizon.com>
2019-09-23 08:01:43 -0500

    initial

---
+++ app.js
@@ -0,0 +1,60 @@
+//Requires Express
+var express = require('express');
+var app = express();
+// Requires body-parser
+var bodyParser = require('body-parser');
+
+//Require database
+const db = require("./models/book").sequelize;
+module.exports = (app, db) => {
+    console.log(books);
+    app.get("/all_books", (req, res) =>
+    db.post.findAll().then((result) => res.json(result))
+    );
+}
+
+// view engine setup
+app.set('view engine', 'pug');
+//Uses body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require("./routes/index"));
+app.use('/books', require("./routes/books"));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err0l
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
